36a9e5053f2070402f53f442d174e9019a499cb7
lina-khaoula <lina.khaoula47@gmail.com>
2026-01-30 23:25:34 +0100

    Tricial comb

---
+++ combinations.py
@@ -0,0 +1,11 @@
+def comb(n, k):
+    if k < 0 or k > n:
+        return 0
+
+    result = 1
+    for i in range(1, k + 1):
+        result = result * (n - i + 1) // i
+    return result
+
+
+print(comb(9,4))
